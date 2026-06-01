4b60e0926d58cb762ca09c32cb8008f73e38121f
Minseo <you@example.com>
2026-06-01 18:05:15 +0200

    importImage function that groups all the image imports together

---
+++ src/Backend/importImage.js
@@ -0,0 +1,61 @@
+import weatherCode0 from "../assets/WeatherCode/weatherCode0.png";
+import weatherCode1 from "../assets/WeatherCode/weatherCode1.png";
+import weatherCode2 from "../assets/WeatherCode/weatherCode2.png";
+import weatherCode3 from "../assets/WeatherCode/weatherCode3.png";
+import weatherCode45 from "../assets/WeatherCode/weatherCode45.png";
+import weatherCode48 from "../assets/WeatherCode/weatherCode48.png";
+import weatherCode51 from "../assets/WeatherCode/weatherCode51.png";
+import weatherCode53 from "../assets/WeatherCode/weatherCode53.png";
+import weatherCode55 from "../assets/WeatherCode/weatherCode55.png";
+import weatherCode56 from "../assets/WeatherCode/weatherCode56.png";
+import weatherCode57 from "../assets/WeatherCode/weatherCode57.png";
+import weatherCode61 from "../assets/WeatherCode/weatherCode61.png";
+import weatherCode63 from "../assets/WeatherCode/weatherCode63.png";
+import weatherCode65 from "../assets/WeatherCode/weatherCode65.png";
+import weatherCode66 from "../assets/WeatherCode/weatherCode66.png";
+import weatherCode67 from "../assets/WeatherCode/weatherCode67.png";
+import weatherCode71 from "../assets/WeatherCode/weatherCode71.png";
+import weatherCode73 from "../assets/WeatherCode/weatherCode73.png";
+import weatherCode75 from "../assets/WeatherCode/weatherCode75.png";
+import weatherCode77 from "../assets/WeatherCode/weatherCode77.png";
+import weatherCode80 from "../assets/WeatherCode/weatherCode80.png";
+import weatherCode81 from "../assets/WeatherCode/weatherCode81.png";
+import weatherCode82 from "../assets/WeatherCode/weatherCode82.png";
+import weatherCode85 from "../assets/WeatherCode/weatherCode85.png";
+import weatherCode86 from "../assets/WeatherCode/weatherCode86.png";
+import weatherCode95 from "../assets/WeatherCode/weatherCode95.png";
+import weatherCode96 from "../assets/WeatherCode/weatherCode96.png";
+import weatherCode99 from "../assets/WeatherCode/weatherCode99.png";
+
+const weatherIcon = {
+  0: weatherCode0,
+  1: weatherCode1,
+  2: weatherCode2,
+  3: weatherCode3,
+  45: weatherCode45,
+  48: weatherCode48,
+  51: weatherCode51,
+  53: weatherCode53,
+  55: weatherCode55,
+  56: weatherCode56,
+  57: weatherCode57,
+  61: weatherCode61,
+  63: weatherCode63,
+  65: weatherCode65,
+  66: weatherCode66,
+  67: weatherCode67,
+  71: weatherCode71,
+  73: weatherCode73,
+  75: weatherCode75,
+  77: weatherCode77,
+  80: weatherCode80,
+  81: weatherCode81,
+  82: weatherCode82,
+  85: weatherCode85,
+  86: weatherCode86,
+  95: weatherCode95,
+  96: weatherCode96,
+  99: weatherCode99,
+};
+
+export { weatherIcon };
--- src/Frontend/App.jsx
+++ src/Frontend/App.jsx
@@ -11,66 +11,7 @@ import {
   weatherCodeToText,
 } from "../Backend/fetchData";
 import { SkeletonImage } from "./util.jsx";
-
-import weatherCode0 from "../assets/WeatherCode/weatherCode0.png";
-import weatherCode1 from "../assets/WeatherCode/weatherCode1.png";
-import weatherCode2 from "../assets/WeatherCode/weatherCode2.png";
-import weatherCode3 from "../assets/WeatherCode/weatherCode3.png";
-import weatherCode45 from "../assets/WeatherCode/weatherCode45.png";
-import weatherCode48 from "../assets/WeatherCode/weatherCode48.png";
-import weatherCode51 from "../assets/WeatherCode/weatherCode51.png";
-import weatherCode53 from "../assets/WeatherCode/weatherCode53.png";
-import weatherCode55 from "../assets/WeatherCode/weatherCode55.png";
-import weatherCode56 from "../assets/WeatherCode/weatherCode56.png";
-import weatherCode57 from "../assets/WeatherCode/weatherCode57.png";
-import weatherCode61 from "../assets/WeatherCode/weatherCode61.png";
-import weatherCode63 from "../assets/WeatherCode/weatherCode63.png";
-import weatherCode65 from "../assets/WeatherCode/weatherCode65.png";
-import weatherCode66 from "../assets/WeatherCode/weatherCode66.png";
-import weatherCode67 from "../assets/WeatherCode/weatherCode67.png";
-import weatherCode71 from "../assets/WeatherCode/weatherCode71.png";
-import weatherCode73 from "../assets/WeatherCode/weatherCode73.png";
-import weatherCode75 from "../assets/WeatherCode/weatherCode75.png";
-import weatherCode77 from "../assets/WeatherCode/weatherCode77.png";
-import weatherCode80 from "../assets/WeatherCode/weatherCode80.png";
-import weatherCode81 from "../assets/WeatherCode/weatherCode81.png";
-import weatherCode82 from "../assets/WeatherCode/weatherCode82.png";
-import weatherCode85 from "../assets/WeatherCode/weatherCode85.png";
-import weatherCode86 from "../assets/WeatherCode/weatherCode86.png";
-import weatherCode95 from "../assets/WeatherCode/weatherCode95.png";
-import weatherCode96 from "../assets/WeatherCode/weatherCode96.png";
-import weatherCode99 from "../assets/WeatherCode/weatherCode99.png";
-
-const weatherIcon = {
-  0: weatherCode0,
-  1: weatherCode1,
-  2: weatherCode2,
-  3: weatherCode3,
-  45: weatherCode45,
-  48: weatherCode48,
-  51: weatherCode51,
-  53: weatherCode53,
-  55: weatherCode55,
-  56: weatherCode56,
-  57: weatherCode57,
-  61: weatherCode61,
-  63: weatherCode63,
-  65: weatherCode65,
-  66: weatherCode66,
-  67: weatherCode67,
-  71: weatherCode71,
-  73: weatherCode73,
-  75: weatherCode75,
-  77: weatherCode77,
-  80: weatherCode80,
-  81: weatherCode81,
-  82: weatherCode82,
-  85: weatherCode85,
-  86: weatherCode86,
-  95: weatherCode95,
-  96: weatherCode96,
-  99: weatherCode99,
-};
+import { weatherIcon } from "../Backend/importImage.js";
 
 function App() {
   const [coord, setCoord] = useState({});
@@ -150,19 +91,23 @@ function App() {
   const dayIndex = getDayIndex(weatherData, date);
 
   return (
-    <div>
-      <nav>
+    <div className="bg-black p-2">
+      <nav className="flex items-center gap-5">
         <a href="#main-weather-display">
           <img src="logo" />
         </a>
-        <input onClick={() => setIsOpen(true)} placeholder="Search..." />
+        <input
+          onClick={() => setIsOpen(true)}
+          placeholder="Search..."
+          className="w-60 h-8 rounded-xl"
+        />
         {isOpen && (
           <div
             className="fixed inset-0 bg-black/40 backdrop-blur-sm flex items-center justify-center"
             onClick={() => {
               setIsOpen(false);
               setSearchText("");
-              setLocationList([]); 
+              setLocationList([]);
             }}
           >
             <div
@@ -195,7 +140,7 @@ function App() {
 
                     setSearchText("");
                     setIsOpen(false);
-                    setLocationList([]); 
+                    setLocationList([]);
                   }}
                   className="border p-2 cursor-pointer hover:bg-gray-100"
                 >
